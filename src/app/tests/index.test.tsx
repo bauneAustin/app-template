dc602811f4f4cd3f04250577e5fc86a45c29e23a
abaune <austinmichaelbaune@gmail.com>
2019-10-27 19:25:06 -0600

    Add test redux

--- src/app/tests/index.test.tsx
+++ src/app/tests/index.test.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react';
 import {shallow} from 'enzyme';
-import App from '../index';
+import {App, mapDispatchToProps, mapStateToProps} from '../index';
 
 describe('App', () => {
+    let props, state, wrapper: any, instance: any;
+
+    const setInstanceAndWrapper = (_props: any, _state: any) => {
+        props = {name: 'test', ..._props};
+
+        state = {test: '', ..._state};
+
+        const appProps = {
+            ...props,
+            mapDispatchToProps: mapDispatchToProps(jasmine.createSpy('Dispatch')),
+            mapStateToProps: mapStateToProps(state)
+        };
+
+        wrapper = shallow(<App {...{...appProps}} />);
+        instance = wrapper.instance();
+    }
+
+    beforeEach(() => {
+        setInstanceAndWrapper({}, {});
+    });
+
+
     it('should shallow render', () => {
-        expect(shallow(<App name={'test'}/>)).toMatchSnapshot();
+        expect(wrapper).toMatchSnapshot();
     });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { testChange } from './reducers/index';
+import { StoreState } from './types/index';
 import App from './app';
+
+const store = createStore<StoreState, any, any, any>(testChange, {
+  test: '',
+});
 
 import './assets/index.scss';
 
-ReactDOM.render(<App name='Test'/>, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={store}>
+        <App name='Test'/>
+    </Provider>, document.getElementById("root") as HTMLElement
+);
